9e1cf47226cc9338ae24d8e1164829b5b7440f73
aleka <psarroa@westminster.ac.uk>
2022-03-25 11:44:19 +0000

    modified in branch

---
+++ 5cosc021w/src/main/java/Welcome.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleka
+ */
+public class Welcome {
+    
+    public static void main(String[] args){
+        System.out.println("Welcome to the 5COSC021W");
+    }
+}
